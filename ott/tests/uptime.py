5d295e1ece3dd8bd0bb5ceadfcc06aa551d5cb56
Frank Purcell <fxpurcell@gmail.com>
2026-02-23 23:54:41 -0800

    fix(uptime): .csv file

--- ott/tests/uptime.py
+++ ott/tests/uptime.py
@@ -1,4 +1,12 @@
+import os
+from ott.utils import file_utils
 from ott.utils import web_utils
 
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+
 def main():
-    web_utils.simple_email("hey", "purcellf@trimet.org")
+    #web_utils.simple_email("hey", "purcellf@trimet.org")
+    p = os.path.join(dir_path, 'uptime.csv')
+    f = file_utils.read_csv(p)
+    print(f)
