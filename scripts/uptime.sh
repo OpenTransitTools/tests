6ade47627b0b1ee752cd388c24541ea8391f506d
Frank Purcell <fxpurcell@gmail.com>
2026-03-07 10:57:17 -0800

    fix(uptime): clean up uptime.sh script to output a heartbeat, other formatting

--- scripts/uptime.sh
+++ scripts/uptime.sh
@@ -1,12 +1,21 @@
+echo -n "."
 rm -f x f
+
 poetry run uptime > x
+sleep 2
+
 grep FAIL x
 fail=$?
 if [ $fail == 0 ]; then
+  echo
+  echo -n "Uptime tests failed! "
   date
-  echo "Uptime tests failed!"
-  awk '{printf "%s\\n", $0}' x > f
   cat x
+  echo
+
+  awk '{printf "%s\\n", $0}' x > f
   $HOME/server-config/chat_bot.sh f
+  sleep 2
 fi
+
 rm -f x f
