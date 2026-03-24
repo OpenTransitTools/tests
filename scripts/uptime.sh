73738d5be4271afac5c2a572b2d390e4f05670fa
Frank Purcell <fxpurcell@gmail.com>
2026-03-24 16:27:43 -0700

    fix(uptime): add urls to Mappy chat

--- scripts/uptime.sh
+++ scripts/uptime.sh
@@ -12,7 +12,7 @@ fail=$?
 if [ $fail == 0 ]; then
   # got an error...send a text to alert folks of the problem
   dt=`date`
-  msg="Uptime tests failed on ${HOSTNAME} at ${dt}: "
+  msg="Uptime tests failed on ${HOSTNAME} at ${dt}:"
 
   echo
   echo "$msg"
@@ -20,7 +20,7 @@ if [ $fail == 0 ]; then
   echo
 
   # format the output for a text, and send it via Mappy
-  echo "$msg" > f
+  echo -n "$msg\n\n" > f
   awk '{printf "%s\\n", $0}' x >> f
   $HOME/server-config/chat_bot.sh f
   sleep 2
